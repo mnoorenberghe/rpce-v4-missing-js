acc5d872daaa0cf4d7dd2d46d8a40df853e72b8c
Matthew Noorenberghe <github@matthew.noorenberghe.com>
2022-01-23 12:51:13 -0500

    MV2 without React

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import react from '@vitejs/plugin-react'
 import { chromeExtension } from 'rollup-plugin-chrome-extension'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  plugins: [react(), chromeExtension({})],
+  plugins: [chromeExtension({})],
 })
